2f41cd4330bb412417aea61b1d26cb5e433a9186
Markeya Mckoy-Carree <mcmcarree@gmail.com>
2022-12-15 13:44:45 -0500

    Events laaab

--- main.js
+++ main.js
@@ -7,3 +7,35 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+const colors = document.querySelectorAll('.color');
+const currentColor = document.querySelector('#current-color');
+
+colors.forEach((color) => {
+  color.addEventListener('click', () => {
+    currentColor.style.background = color.style.background;
+  })
+})
+
+const cells = document.querySelectorAll('.cell');
+
+cells.forEach((cell) => {
+  cell.addEventListener('click', () => {
+    //cell.addEventListener('mousedown', () => {
+    cell.style.background = currentColor.style.background;
+  })
+})
+
+const resetButton = document.createElement("div");
+resetButton.textContent = `🆑`;
+document.querySelector("#canvas").append(resetButton);
+resetButton.addEventListener("click", () => {
+  cells.forEach((cell) => (cell.style.background = "none"));
+});
+
+const fillButton = document.createElement("div");
+fillButton.textContent = `⛽️`;
+document.querySelector("#canvas").append(fillButton);
+fillButton.addEventListener("click", () => {
+  cells.forEach((cell) => (cell.style.background = "red"));
+});
+
